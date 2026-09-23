45118526f263ae78f4a715a681d701c771e62670
Shivamyadav8182 <shivamydv.work25@gmail.com>
2026-09-24 00:01:54 +0530

    Complete Plan With Shivam project

---
+++ src/data.js
@@ -0,0 +1,53 @@
+const data = [
+	{
+		id: 1,
+		name: "Agra",
+		info: "Agra is a city in northern India, known for the iconic Taj Mahal, a stunning white marble mausoleum built by Mughal emperor Shah Jahan in memory of his beloved wife. Agra is also home to several other historic and architectural wonders, including Agra Fort and Fatehpur Sikri.",
+		image: "https://images.unsplash.com/photo-1564507592333-c60657eea523",
+		price: "₹35,758",
+	},
+	{
+		id: 2,
+		name: "Jaipur",
+		info: `Jaipur is the capital city of the Indian state of Rajasthan, known for its stunning pink-hued buildings and palaces. The city is also known as the "Pink City" due to the color of many of its buildings. Jaipur is famous for its intricate textiles, colorful handicrafts, and delicious cuisine.`,
+		image:  "https://www.sundaytour.com.tw/upfiles/chinese/attractions/tw_attractions_caty01594955955.jpg",
+		price: "₹82,560",
+	},
+	{
+		id: 3,
+		name: "Goa",
+		info: " Goa is a small state on India's western coast, known for its stunning beaches, vibrant nightlife, and Portuguese-influenced architecture. The state is also known for its spicy seafood dishes and unique cultural traditions.",
+		image: "https://cdn.travelcoffee.in/uploads/a966817f-ffcc-40f7-94c1-1bf7079d3392-Goa_Weekend_Getaway.webp",
+		price: "₹29,695",
+	},
+	{
+  id: 4,
+  name: "Varanasi",
+  price: "₹31,095",
+  info: "Varanasi is one of the oldest cities in India and is famous for its beautiful ghats along the Ganga River. Visitors can experience the peaceful Ganga Aarti, explore the historic lanes near the river, and visit important temples such as the Kashi Vishwanath Temple.",
+  image:  "https://thumb.wikimedia.org/wikipedia/commons/thumb/5/53/Varanasi%2C_India%2C_Ghats%2C_temples_and_embankments.jpg/1920px-Varanasi%2C_India%2C_Ghats%2C_temples_and_embankments.jpg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=thumbnail&_=20210602110515",
+},
+	{
+  id: 5,
+  name: "Darjeeling",
+  price: "₹78,695",
+  info: "This includes Tiger Hill, Kanchenjunga, tea gardens, and the Darjeeling Himalayan Railway, all of which are highlighted by the Darjeeling district's official tourism information.",
+  image: "https://neptuneholidays.com/blog/images/blog/kanchenjunga_form_tiger_hill.jpg"
+},
+	{
+		id: 6,
+		name: "Jaisalmer",
+		info: "Jaisalmer is a city in the Indian state of Rajasthan, known for its stunning sandstone architecture and its location in the heart of the Thar Desert. ",
+		image:  "https://in-exstatic-vivofs.vivo.com/bCy1HLzT9bUXp6Mb/IN-IQOO/threadResource/20251028/5daa82563224460798d8c45ec85a1320_w3264_h2448.jpg_q1.webp",
+		price: "₹68,595",
+	},
+	{
+		id: 7,
+		name: "Kochi",
+		info: "Kochi is a port city in the Indian state of Kerala, known for its historic connections to trade and commerce. The city is home to several stunning churches, synagogues, and mosques, as well as a rich culinary tradition that blends Indian, Portuguese, and other influences.",
+		image: "https://media.assettype.com/outlooktraveller/2024-11-05/l1gt0s7z/shutterstock2348038323.jpg",
+		price: "₹68,595",
+	},
+];
+
+export default data;
